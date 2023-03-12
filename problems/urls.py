bdba2ac63792e46bd496f683348832b6307bda71
gyubinheo <gjrbqls95@naver.com>
2023-03-12 19:28:39 +0900

    ADD: serializers, views, fat models

--- problems/urls.py
+++ problems/urls.py
@@ -1,3 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from . import views
 
-urlpatterns = []
+router = routers.DefaultRouter()
+router.register(r"problems", views.ProblemViewSet)
+router.register(r"answers", views.AnswerViewSet)
+router.register(r"explanations", views.ExplanationViewSet)
+
+urlpatterns = [
+    path("api/", include(router.urls)),
+    path("api/category/", views.CategoryListView.as_view()),
+]
--- problems/views.py
+++ problems/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.core.cache import cache
+from rest_framework import viewsets, generics
+from rest_framework.response import Response
+from .models import Category, Problem, Answer, Explanation
+from .serializers import (
+    CategorySerializer,
+    ProblemSerializer,
+    AnswerSerializer,
+    ExplanationSerializer,
+)
 
-# Create your views here.
+
+class CategoryListView(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class ProblemViewSet(viewsets.ModelViewSet):
+    queryset = Problem.objects.all()
+    serializer_class = ProblemSerializer
+
+    def list(self, request):
+        problems = cache.get("problems")
+        if not problems:
+            problems = Problem.objects.all()
+            cache.set("problems", problems, 60 * 60)
+        serializer = ProblemSerializer(problems, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, *args, **kwargs):
+        problem = self.get_object()
+        serializer = self.get_serializer(problem)
+        serializer.data["answer"] = problem.get_answer()
+        serializer.data["explanation"] = problem.get_explanation()
+        return Response(serializer.data)
+
+
+class AnswerViewSet(viewsets.ModelViewSet):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
+
+
+class ExplanationViewSet(viewsets.ModelViewSet):
+    queryset = Explanation.objects.all()
+    serializer_class = ExplanationSerializer
