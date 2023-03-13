a6e4e12a527888e4854cdb480cc1a05c14b34b27
gyubinheo <gjrbqls95@naver.com>
2023-03-13 21:26:39 +0900

    ADD: settings redis cache

--- config/settings.py
+++ config/settings.py
@@ -134,3 +134,16 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Cache
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+    }
+}
