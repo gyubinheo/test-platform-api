34fb161254003704d63aa4b9788e0de929b951f1
gyubinheo <gjrbqls95@naver.com>
2023-03-16 00:31:18 +0900

    ADD: ProblemDetailSerializer update method

--- problems/serializers.py
+++ problems/serializers.py
@@ -43,6 +43,28 @@ class ProblemDetailSerializer(serializers.ModelSerializer):
         Explanation.objects.create(problem=problem, **explanation_data)
         return problem
 
+    def update(self, instance, validated_data):
+        answer_data = validated_data.pop("answer", None)
+        explanation_data = validated_data.pop("explanation", None)
+
+        instance.category = validated_data.get("category", instance.category)
+        instance.title = validated_data.get("title", instance.title)
+        instance.description = validated_data.get("description", instance.description)
+        instance.difficulty = validated_data.get("difficulty", instance.difficulty)
+        instance.created_by = validated_data.get("created_by", instance.created_by)
+        instance.updated_at = validated_data.get("updated_at", instance.updated_at)
+        instance.save()
+
+        if answer_data:
+            answer_serializer = self.fields["answer"]
+            answer_serializer.update(instance.answer, answer_data)
+
+        if explanation_data:
+            explanation_serializer = self.fields["explanation"]
+            explanation_serializer.update(instance.explanation, explanation_data)
+
+        return instance
+
     class Meta:
         model = Problem
         fields = [
