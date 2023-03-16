25ec6d1f391fded69d4e7e8f644dfeff16c033fa
gyubinheo <gjrbqls95@naver.com>
2023-03-16 15:07:48 +0900

    ADD: swagger settings

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
     path("", include("problems.urls")),
     path("submissions/", include("submissions.urls")),
     # path("accounts/", include("accounts.urls")),
